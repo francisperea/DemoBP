40bf48d80ef744da9228977b9d99451774486485
Francis Perea <francis@francisperea.org>
2012-06-18 23:15:13 +0200

    Mejora del sistema de mostrado/ocultado de burbujas del menu
    Se abren y cierran al click

--- js/init.js
+++ js/init.js
@@ -50,26 +50,70 @@ $(function(){
 		shadowFrom: false
 	})
 	
-	// Configuración de las burbujas 
+	// Configuración de las burbujas del menú izquierdo
+	$('.menu_izdo .boton a').CreateBubblePopup({ 
+		innerHtmlStyle: 	  	{ color:'#5e5e5e', 'text-align':'center', 'font-size': '12px'},
+		themeName: 	  	 	'azure',
+		themePath: 	  	 	'images/jquerybubblepopup-themes',							  						 
+		manageMouseEvents:	false,
+		position: 'bottom',
+		align: 'center'
+	});
+	
+	$('.menu_izdo .boton a').click(function(){
+			if( !$(this).data('visible') ){
+			
+				$('.menu_izdo .boton a').data('visible', false);
+				$(this).data('visible', true);
+				
+				$('.menu_izdo .boton a').HideAllBubblePopups();
+				
+				var tipcontent = $(this).parent().next('.tipcontent').html();
+				if (tipcontent)
+					$(this).SetBubblePopupInnerHtml( tipcontent );
+				else
+					$(this).SetBubblePopupInnerHtml( $(this).attr('alt') );
+				$(this).ShowBubblePopup();
+				
+				var img_position = $(this).parent().prev().offset();
+				var img_top = parseInt(img_position.top);
+				var img_left = parseInt(img_position.left);
+				var area_x = parseInt($(this).attr('coords').split(',')[0]);
+				var area_y = parseInt($(this).attr('coords').split(',')[3]);
+				var area_width = parseInt($(this).attr('coords').split(',')[2] - $(this).attr('coords').split(',')[0]);
+				var bubble_width  = parseInt($('#'+$(this).GetBubblePopupID()).outerWidth(false));
+				var bubble_height = parseInt($('#'+$(this).GetBubblePopupID()).outerHeight(false));
+			
+				$('#'+$(this).GetBubblePopupID()).css({ top: (area_y+img_top)+'px', left: (area_x+img_left+Math.abs(area_width/2)-Math.abs(bubble_width/2))+'px' });
+			
+			}
+			else {
+			$('.menu_izdo .boton a').HideAllBubblePopups();
+			$('.menu_izdo .boton a').data('visible', false);
+		};
+	});
+	
+	
+	
+	// Configuración de las burbujas de las zonas calientes
 	$('area').css({ display: 'block'});
 	$('area').each(function(){
 		$(this).offset({ top: $(this).parent().prev().offset().top, left: $(this).parent().prev().offset().left });	
 	});
 	
-	$('area').CreateBubblePopup({ innerHtmlStyle: 	  	{ color:'#5e5e5e', 'text-align':'center', 'font-size': '12px'},
-								  themeName: 	  	 	'azure',
-								  themePath: 	  	 	'images/jquerybubblepopup-themes',							  						 
-								  manageMouseEvents:	false,
-								  position: 'bottom',
-								  align: 'center'
-							   });
+	$('area').CreateBubblePopup({ 
+		innerHtmlStyle: 	  	{ color:'#5e5e5e', 'text-align':'center', 'font-size': '12px'},
+		themeName: 	  	 	'azure',
+		themePath: 	  	 	'images/jquerybubblepopup-themes',							  						 
+		manageMouseEvents:	false,
+		position: 'bottom',
+		align: 'center'
+	});
 
 	$('area').data('visible', false);
 	
 	var timer;
 	$('area').mouseover(function(){
-	
-			clearTimeout(timer);
 	
 			if( !$(this).data('visible') ){
 			
@@ -99,7 +143,8 @@ $(function(){
 			};
 	});
 
-	$('area').mouseleave(function(){
+	/*
+$('area').mouseleave(function(){
 
 		if( $(this).data('visible') ){
 			var seconds_to_wait = 1;
@@ -119,8 +164,10 @@ $(function(){
 		};
 		
 	});
-	var ancho;
+*/
+	
 	// Configuración botonera desplegable
+	var ancho;
 	$('.despliegue').click(function(){
 		if ($('.despliegue').hasClass('cerrado')) {
 			// Abre
